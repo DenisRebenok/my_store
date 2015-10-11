5da2c361a18b9789fda847a886037eaa3b0bcd88
DenisRebenok <denis.rebenok.pro@gmail.com>
2015-10-11 03:07:28 +0300

    helpers testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def urls_to_images(s)
+    unless s.nil?
+      s.gsub! /\s(http:\/\/.*?(png|jpg))/,
+          '<p><img src="\1"/></p>'
+      s.html_safe
+    end
+  end
+
+  def urls_to_links(s)
+    unless s.nil?
+      s.gsub! /\s(http:\/\/.*?)\s/,
+              '<a href="\1"/>\1</a>'
+      s.html_safe
+    end
+  end
+
 end
